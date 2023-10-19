dcab867500b701e4add5af0665fe6e8436d3d33c
Moreno <morenoschenk@gmail.com>
2023-10-19 15:01:17 +0200

    Initial commit

---
+++ BlazorApp1/Data/MyErrorBoundary.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorApp1.Data;
+
+public class MyErrorBoundary : ErrorBoundary
+{
+    public new Exception? CurrentException => base.CurrentException;
+}
